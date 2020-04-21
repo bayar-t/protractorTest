0e453abe1a3e563763c6fd96627736cc22e6ceba
Bayar Tsogtbaatar <stsogtba@ucsd.edu>
2020-04-20 18:14:46 -0700

    Add files via upload

---
+++ protractor/conf.js
@@ -0,0 +1,10 @@
+exports.config = {
+   seleniumAddress: 'http://localhost:4444/wd/hub',
+   //directConnect: true,
+   specs: ['todo-spec.js']
+ //  capabilities: {
+ //     'browserName': 'chrome',
+ //  },
+ //  useAllAngular2AppRoots: true,
+ //  framework: 'jasmine'
+};
